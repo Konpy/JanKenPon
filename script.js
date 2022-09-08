d5c17afc71716e0c1027791c2eb11d215dc91afb
Konpy <fontanezgabriel7@gmail.com>
2022-09-08 13:02:53 -0400

    finished the script logic. Will have to change the user input later

--- script.js
+++ script.js
@@ -1,18 +1,71 @@
+/*pick between rock paper or scissors as a string*/
 function compChoice (){
     var choices = Array('rock','paper','scissors')
     const option = Math.floor(Math.random()*choices.length);
-    console.log(option)
     return choices[option];
 }
+/*For now this is fine. Itll need to be changed so it takes buttons as input instead*/ 
+function userChoice(){
+    let chosen = prompt("Select a target for battle");
+    let lowerUser = chosen.toLowerCase();
+    return lowerUser;
+}
+/*RPS game logic*/
 function game(){
-    let userChoice = prompt("Select a target for battle");
-    let lowerUser = userChoice.toLowerCase();
-    let comp = compChoice();
+    var userScore = 0;
+    var compScore = 0;
 
-    if (lowerUser == comp){
-        console.log("We have the same thing");
-    }
-    else{
-        console.log("Not same")
+    for(let i = 0; i < 10; i++)
+    {
+        let user = userChoice();
+        let comp = compChoice();   
+        if (userScore === 5 || compScore === 5){break;}
+        
+        if (user === 'rock'){
+            if (comp == 'scissors'){
+                document.getElementById("Output").innerHTML = "Won round";
+                console.log("Won round");
+                userScore += 1;
+            }
+            else if (comp == 'paper'){
+                console.log("Lost round");
+                document.getElementById("Output").innerHTML = "Lost round";
+                compScore += 1;
+            }
+            else{
+                console.log("It's a tie");
+            }
+        }
+        if (user === 'scissors'){
+            if (comp == 'paper'){
+                console.log("Won round");
+                document.getElementById("Output").innerHTML = "Won round";
+                userScore += 1;
+            }
+            else if (comp == 'rock'){
+                console.log("Lost round");
+                document.getElementById("Output").innerHTML = "Lost round";
+                compScore += 1;
+            }
+            else{
+                console.log("It's a tie");
+            }     
+        }
+        if (user === 'paper'){
+            if (comp == 'rock'){
+                console.log("Won round");
+                document.getElementById("Output").innerHTML = "Won round";
+                userScore += 1;
+            }
+            else if (comp == 'scissors'){
+                console.log("Lost round");
+                document.getElementById("Output").innerHTML = "Lost round";
+                compScore += 1;
+            }
+            else{
+                console.log("It's a tie");
+            }
+        }  
+        console.log(userScore);
     }
 }
